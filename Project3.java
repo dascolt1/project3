c16cd627cf17e3fb9fbc25dbbe1df2fa2f63e9a3
dascolt1 <dascolt1@tcnj.edu>
2022-11-29 14:32:19 -0500

    hastable implemented

--- Project3.java
+++ Project3.java
@@ -16,14 +16,17 @@ class Project3 {
             String line;
             String[] arrOfStr = new String[100];
             while ((line = br.readLine()) != null) {
-                arrOfStr = line.split(", ", 5);
-                System.out.println(line);
+                arrOfStr = line.split(", ", 0);
+                ht.put(arrOfStr[0], arrOfStr[1]);
             }
+
         } catch (FileNotFoundException ex) {
             System.out.println(ex);
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        System.out.println(ht.get("oo.oneal@stevens.edu"));
 
     }
 }
