ca60abe636795fb6c66907c1b9a1f735b025a005
dascolt1 <dascolt1@tcnj.edu>
2022-11-29 22:34:59 -0500

    more plumbing

--- Project3.java
+++ Project3.java
@@ -1,3 +1,8 @@
+/*
+* Professor Reid CS 520
+* Tommy D'Ascoli and Prashant Patil
+*/
+
 import java.util.*;
 import java.io.*;
 
@@ -6,17 +11,36 @@ class Project3 {
     private static void readLoginFile(String filePath, Hashtable<String, String> ht) {
         try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
-            String[] arrOfStr = new String[100];
+            String[] arrOfStr = new String[2];
             while ((line = br.readLine()) != null) {
                 // gets username/password pairs
                 arrOfStr = line.split(", ", 0);
                 // adds them to hashtable
                 ht.put(arrOfStr[0], arrOfStr[1]);
             }
-        } catch (FileNotFoundException ex) {
-            ex.printStackTrace();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void writeLogFile(String filePath, String logOutput) {
+        // TODO implement file writing
+    }
+
+    private static void handleLogin(String filePath, boolean login) {
+        int numberOfTries = 3;
+        if (login == true) {
+            System.out.println("Login Successful");
+            writeLogFile(filePath, "Test");
+            System.exit(0);
+        } else {
+            System.out.println("Username or password incorrect.");
+            numberOfTries--;
+            if (numberOfTries == 0) {
+                System.out.println("Account locked, please try again in an hour.");
+            }
         }
     }
 
@@ -24,7 +48,16 @@ class Project3 {
         Hashtable<String, String> hashTable = new Hashtable<String, String>();
         String fileToRead = "LoginsAndPasswords.txt";
         String fileToWrite = "signIn.txt";
+
+        String username = "jjohns@stevens.edu";
+        String password = "0ski22";
+
         readLoginFile(fileToRead, hashTable);
 
+        if (hashTable.get(username).equals(password)) {
+            handleLogin(fileToWrite, true);
+        } else {
+            handleLogin(fileToWrite, false);
+        }
     }
 }
