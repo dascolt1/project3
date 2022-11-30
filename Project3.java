450e7485746a06251c3d6d97dcf4331cc2b81de2
dascolt1 <dascolt1@tcnj.edu>
2022-11-29 22:15:11 -0500

    cleaned up code into functions

--- Project3.java
+++ Project3.java
@@ -3,16 +3,8 @@ import java.io.*;
 
 class Project3 {
 
-    private void readLoginFile(String filePath) {
-
-    }
-
-    public static void main(String args[]) throws FileNotFoundException {
-        Hashtable<String, String> ht = new Hashtable<String, String>();
-        String fileToRead = "LoginsAndPasswords.txt";
-        String fileToWrite = "signIn.txt";
-
-        try (BufferedReader br = new BufferedReader(new FileReader(fileToRead))) {
+    private static void readLoginFile(String filePath, Hashtable<String, String> ht) {
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
             String line;
             String[] arrOfStr = new String[100];
             while ((line = br.readLine()) != null) {
@@ -21,14 +13,18 @@ class Project3 {
                 // adds them to hashtable
                 ht.put(arrOfStr[0], arrOfStr[1]);
             }
-
         } catch (FileNotFoundException ex) {
-            System.out.println(ex);
+            ex.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-        System.out.println(ht.get("oo.oneal@stevens.edu"));
+    public static void main(String args[]) throws FileNotFoundException {
+        Hashtable<String, String> hashTable = new Hashtable<String, String>();
+        String fileToRead = "LoginsAndPasswords.txt";
+        String fileToWrite = "signIn.txt";
+        readLoginFile(fileToRead, hashTable);
 
     }
 }
